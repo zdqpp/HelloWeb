87c637441bc1aacca1dc39c4e3b6b2737654bacf
zhengdaqiang <zhengdaqiang@chinasofti.com>
2019-02-01 14:19:40 +0800

    建仓库

---
+++ src/com/yiibai/springmvc/HelloController.java
@@ -0,0 +1,24 @@
+package com.yiibai.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @RequestMapping("/get")
+    public String printHello(ModelMap model) {
+        model.put("message", "Hello Spring MVC Framework");
+        return "hello";
+    }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello1(ModelMap model) {
+        model.put("message", "Hello Spring MVC");
+        return "hello";
+    }
+
+}
