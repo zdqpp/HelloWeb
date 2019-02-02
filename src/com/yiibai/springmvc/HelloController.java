037efa8080b8d4f2e1fc04d2d354a7dc5eee9466
zhengdaqiang <zhengdaqiang@chinasofti.com>
2019-02-02 11:20:14 +0800

    test

--- src/com/yiibai/springmvc/HelloController.java
+++ src/com/yiibai/springmvc/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @RequestMapping("/get")
     public String printHello(ModelMap model) {
-        model.put("message", "Hello Spring MVC Frameworkddd");
+        model.put("message", "Hello Spring MVC Frameworkeee");
         return "hello";
     }
     
